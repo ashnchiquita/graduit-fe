f2c14ca4ddd59f43f334941b79674faf6b89f796
arieljovananda88 <jovanandaa@gmail.com>
2024-03-21 07:14:38 +0700

    feat: data table for log bimbingan mahasiswa

--- src/pages/log/bimbingan-mahasiswa/LogBimbinganMahasiswa.tsx
+++ src/pages/log/bimbingan-mahasiswa/LogBimbinganMahasiswa.tsx
@@ -1,81 +1,24 @@
-"use client";
 import { DataTable } from "@/components/DataTable";
-import type { SystemLogs } from "@/lib/entity";
-import {
-  ColumnDef,
-  PaginationState,
-  SortingState,
-  getCoreRowModel,
-  getPaginationRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import { useEffect, useState } from "react";
-
-const columns: ColumnDef<SystemLogs>[] = [
-  {
-    header: "User ID",
-    accessorKey: "id",
-    minSize: 1000,
-  },
-  {
-    header: "Action",
-    accessorKey: "action",
-    minSize: 1000,
-  },
-  {
-    header: "Time Stamp",
-    accessorKey: "createdAt",
-    minSize: 1000,
-  },
-];
+import { useState } from "react";
+import useLogBimbinganMahasiswa from "./hooks/useLogBimbinganMahasiswa";
 
 export default function LogBimbinganMahasiswa() {
-  const data: SystemLogs[] = [
-    {
-      id: "1",
-      idPengguna: "2",
-      action: "Ada deh",
-      createdAt: "02/03/2024 19:58",
-    },
-  ];
-
-  const [pagination, setPagination] = useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: 10,
-  });
-
-  const [sorting, setSorting] = useState<SortingState>([
-    {
-      id: "id",
-      desc: false,
-    },
-  ]);
-
-  useEffect(() => {
-    console.log(sorting);
-  }, [sorting]);
-
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onPaginationChange: setPagination,
-    onSortingChange: setSorting,
-    state: {
-      pagination,
-      sorting,
-    },
-  });
-
   const [searchValue, setSearchValue] = useState("");
+  const { table } = useLogBimbinganMahasiswa()
 
   return (
     <main className="flex min-h-screen flex-col p-5">
+      {/* <p className="mb-10 text-5xl font-black">Bimbingan Logs</p> */}
       <DataTable
         table={table}
-        searchValue={searchValue}
+        headline="Log Bimbingan"
+        description="Berikut merupakan log bimbingan"
         setSearchValue={setSearchValue}
+        searchValue={searchValue}
+        searchPlaceholder="Search something"
+        onClickFilter={() => {
+          console.log("filtered");
+        }}
       />
     </main>
   );
---
+++ src/pages/log/bimbingan-mahasiswa/components/StatusCircle.tsx
@@ -0,0 +1,20 @@
+import { RiCheckboxCircleFill } from "react-icons/ri";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { Row } from "@tanstack/react-table";
+import { BimbinganLogs } from "../types";
+
+export default function StatusCircle({
+    row,
+  }: {
+    row: Row<BimbinganLogs>;
+  }): JSX.Element {
+    return(
+        <div className="flex justify-center">
+            {row.original.status ?
+                <RiCheckboxCircleFill className="text-green-400"/> :
+                <RiCloseCircleFill className="text-red-600"/>
+            }
+        </div>
+    )
+}
+
