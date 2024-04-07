5e115207353949c9f274efd3bc31bc48b51b98b1
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-04-08 00:23:54 +0700

    feat: env validation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { ValidateEnv } from "@julr/vite-plugin-validate-env";
+import { z } from "zod";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ValidateEnv({
+      debug: true,
+      validator: "zod",
+      schema: {
+        VITE_S1_API_URL: z.string().url(),
+        VITE_LOGIN_API_URL: z.string().url(),
+        VITE_S2_API_URL: z.string().url(),
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
