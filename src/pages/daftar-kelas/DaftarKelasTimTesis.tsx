82ad1d52dc0976386473dd443619ea9a46a11a5e
Alifioditya <alifioditya@gmail.com>
2024-04-12 23:54:14 +0700

    fix: daftar kelas UI grid

--- src/pages/daftar-kelas/DaftarKelasTimTesis.tsx
+++ src/pages/daftar-kelas/DaftarKelasTimTesis.tsx
@@ -76,7 +76,7 @@ export default function DaftarKelasTimTesis(): JSX.Element {
       </div>
 
       {/* Daftar Kelas */}
-      <div className="grid w-full grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      <div className="grid w-fit grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {daftarKelas.map((kelas) => (
           <div onClick={() => {}} className="mx-auto max-w-xs cursor-pointer">
             <CardKelas key={kelas.id} dataKelas={kelas} />
