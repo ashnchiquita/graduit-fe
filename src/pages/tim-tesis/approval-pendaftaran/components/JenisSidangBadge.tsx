45906a52cb38bc2863bcdb9430564e5cdd810d60
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2024-05-26 18:34:35 +0700

    feat: wiring s2 rekap sidsem

--- src/pages/tim-tesis/approval-pendaftaran/components/JenisSidangBadge.tsx
+++ src/pages/tim-tesis/approval-pendaftaran/components/JenisSidangBadge.tsx
@@ -9,9 +9,9 @@ export default function JenisSidangBadge({
     <div
       className={clsx(
         "flex w-fit items-center gap-1 rounded-xl px-2 py-1",
-        jenis === "Sidang Tesis"
+        jenis === "Sidang Tesis" || jenis === "SIDANG"
           ? "bg-green-700"
-          : jenis === "Seminar Proposal"
+          : jenis === "Seminar Proposal" || jenis === "SEMINAR_1"
             ? "bg-blue-600"
             : "bg-indigo-400",
       )}
@@ -19,9 +19,9 @@ export default function JenisSidangBadge({
       <div
         className={clsx(
           "aspect-square size-1 rounded-full",
-          jenis === "Sidang Tesis"
+          jenis === "Sidang Tesis" || jenis === "SIDANG"
             ? "bg-green-900"
-            : jenis === "Seminar Proposal"
+            : jenis === "Seminar Proposal" || jenis === "SEMINAR_1"
               ? "bg-blue-900"
               : "bg-indigo-600",
         )}
