8041dd9618a694c3ba10af4f724f34b3b337037d
Alifioditya <alifioditya@gmail.com>
2024-06-10 05:24:24 +0700

    fix: download box on sidsem detail

--- src/pages/tim-tesis/detail-pengajuan/components/ButtonDownload.tsx
+++ src/pages/tim-tesis/detail-pengajuan/components/ButtonDownload.tsx
@@ -1,7 +1,8 @@
 import { HTMLAttributes, forwardRef } from "react";
 import { cn } from "@/lib/utils";
-import { AiOutlineCopy, AiOutlineCloudDownload } from "react-icons/ai";
+import { AiOutlineCopy } from "react-icons/ai";
 import { Berkas } from "../type";
+import { FiExternalLink } from "react-icons/fi";
 interface ButtonDownloadProps extends HTMLAttributes<HTMLDivElement> {
   data: Berkas[];
 }
@@ -26,9 +27,14 @@ export const ButtonDownload = forwardRef<HTMLDivElement, ButtonDownloadProps>(
                 <p className="text-xs font-medium text-[#475569]">
                   {item.nama}
                 </p>
-                <a href={item.link} className="flex items-center gap-1">
-                  <AiOutlineCloudDownload className="text-sm text-blue-500" />
-                  <p className="text-xs font-medium text-blue-500">Download</p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={item.link}
+                  className="flex items-center gap-1"
+                >
+                  <FiExternalLink className="size-3.5 text-[#0070FF]" />
+                  <p className="text-xs font-medium text-blue-500">Lihat</p>
                 </a>
               </div>
             </div>
