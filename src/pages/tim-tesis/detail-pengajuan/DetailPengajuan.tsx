8a04ac522e79ef61913e186f56e420a5ba5ae6e0
Fajar Herawan <112335075+fajarmhrwn@users.noreply.github.com>
2024-06-10 09:35:45 +0700

    Wiring Dashboard TIMTA

--- src/pages/tim-tesis/detail-pengajuan/DetailPengajuan.tsx
+++ src/pages/tim-tesis/detail-pengajuan/DetailPengajuan.tsx
@@ -340,7 +340,7 @@ export default function DetailPengajuan() {
                           {"Ubah"}
                         </Button>
                       )}
-                  </>
+                    </>
                   }
                 />
               </div>
